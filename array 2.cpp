7897155ddfc4061b6c1891c7d178666cbe009a67
fitryoktavia <58461668+fitryoktavia@users.noreply.github.com>
2019-12-03 16:51:02 +0800

    Tugas Alprog 4 C++

---
+++ array 2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int bil[4]={4,5,6,7};
+	cout<<"Penjumlahan :";
+	cout<<bil[0]+bil[1]+bil[2]+bil[3];
+	cout<<endl;
+	cout<<"Hasil Perkalian : ";
+	cout<<bil[0]*bil[1]*bil[2]*bil[3];
+	return 0;
+}
