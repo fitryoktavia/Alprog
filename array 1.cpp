e874e2db9b7b153d1d26e8ac65838d3b997a9213
fitryoktavia <58461668+fitryoktavia@users.noreply.github.com>
2019-12-03 16:50:18 +0800

    Tugas Alprog 4 C++

---
+++ array 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int varray[6]={7,5,7,2,6,3};
+	cout<<varray[0];
+	cout<<endl;
+	cout<<varray[1];
+	cout<<endl;
+	cout<<varray[2];
+	cout<<endl;
+	cout<<varray[3];
+	cout<<endl;
+	cout<<varray[4];
+	cout<<endl;
+	cout<<varray[5];
+	cout<<endl;
+}
+
